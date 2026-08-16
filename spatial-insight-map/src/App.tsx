89a17da0d2a9b088916de8f6ce474bec9dd5a360
jihyun4869 <b0309209300@gmail.com>
2026-08-16 14:13:40 +0900

    Update App.tsx

--- spatial-insight-map/src/App.tsx
+++ spatial-insight-map/src/App.tsx
@@ -12,7 +12,7 @@ import { SavePresetModal } from './components/SavePresetModal';
 import { getNextPresetName } from './utils/presetUtils';
 
 // 🔑 비밀번호 설정 (필요에 따라 변경 가능)
-const ACCESS_PASSWORD = '1234';
+const ACCESS_PASSWORD = '037275';
 
 const DEFAULT_INITIAL_PRESET: ColoringPreset = {
   id: 'preset_default_1',
